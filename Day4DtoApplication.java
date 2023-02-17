6caef1b3999d73f7ba7078afe4f882014bf31a29
vmane0310 <125656475+vmane0310@users.noreply.github.com>
2023-02-17 16:49:43 +0530

    Add files via upload

---
+++ Day4DtoApplication.java
@@ -0,0 +1,13 @@
+package com.dtosamp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4DtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4DtoApplication.class, args);
+	}
+
+}
